dba424f1986d6b8918b1c38a3fa3d96b74cb7716
therohitjha <therohitjha@gmail.com>
2023-02-01 17:33:23 +0530

    revolist grid package added

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,8 @@
 import { Component, h, Listen, State } from '@stencil/core';
 import Papa from 'papaparse';
+import { applyPolyfills, defineCustomElements } from '@revolist/revogrid/loader';
+applyPolyfills();
+defineCustomElements();
 
 @Component({
   tag: 'my-component',
@@ -8,29 +11,28 @@ import Papa from 'papaparse';
 })
 export class MyComponent {
   @State() results: any = [];
+  @State() columns: any[] = [];
   @State() fileName: string[] = [];
   @State() activeIndex: number = 0;
   @State() editMode: boolean = false;
   @State() selectedColumnIndex: number = -1;
   @State() startIndex: number = 0;
+  @State() loader: boolean = false;
   @State() endIndex: number = 30;
-  @State() lastScrollTop: number = 0;
-  @State() rowHeight: number = 22;
   copiedData: any[] = [];
   tableDivRef: HTMLDivElement;
   rowContainerRef: HTMLDivElement;
-  // @Element() el: HTMLDivElement;
-  // observer: IntersectionObserver;
 
   @Listen('change', { capture: true })
   handleFiles(event: any) {
+    this.loader = true;
     const files = event.target.files;
     if (files.length) {
       for (let i = 0; i < files.length; i++) {
         if ((this.fileName.length === 0 && this.results.length === 0) || !this.fileName.some((name: string) => name === files[i].name)) {
           this.fileName = this.fileName.length > 0 ? [...this.fileName, files[i].name] : [files[i].name];
           Papa.parse(files[i], {
-            // header: true,
+            header: true,
             complete: results => {
               this.results = this.results.length > 0 ? [...this.results, results.data] : [results.data];
             },
@@ -38,38 +40,39 @@ export class MyComponent {
         }
       }
     }
+    this.loader = false;
   }
 
-  getHeader(length: number): string[] {
-    let headerData: string[] = [];
-    for (let index = 0; index < length; index++) {
-      headerData.push(String.fromCharCode(65 + index));
-    }
-    return headerData;
-  }
+  // getHeader(length: number): string[] {
+  //   let headerData: string[] = [];
+  //   for (let index = 0; index < length; index++) {
+  //     headerData.push(String.fromCharCode(65 + index));
+  //   }
+  //   return headerData;
+  // }
 
   activeTab(index: number) {
     this.activeIndex = index;
   }
 
-  editTableData(e: any, td: string | number, th: string | number, index: number, rowOrColumn: string) {
-    if (e.target.textContent !== td) {
-      if (rowOrColumn === 'row') {
-        if (td === this.results[this.activeIndex].slice(1)[index][th]) {
-          this.results[this.activeIndex].slice(1)[index][th] = e.target.textContent;
-        }
-      } else {
-        if (td === this.results[this.activeIndex][0][th]) {
-          this.results[this.activeIndex][0][th] = e.target.textContent;
-        }
-      }
-    }
-    this.editMode = false;
-  }
+  // editTableData(e: any, td: string | number, th: string | number, index: number, rowOrColumn: string) {
+  //   if (e.target.textContent !== td) {
+  //     if (rowOrColumn === 'row') {
+  //       if (td === this.results[this.activeIndex].slice(1)[index][th]) {
+  //         this.results[this.activeIndex].slice(1)[index][th] = e.target.textContent;
+  //       }
+  //     } else {
+  //       if (td === this.results[this.activeIndex][0][th]) {
+  //         this.results[this.activeIndex][0][th] = e.target.textContent;
+  //       }
+  //     }
+  //   }
+  //   this.editMode = false;
+  // }
 
-  activateEditMode() {
-    this.editMode = true;
-  }
+  // activateEditMode() {
+  //   this.editMode = true;
+  // }
 
   // componentDidUpdate() {
   //   if (this.selectedColumnIndex !== -1) {
@@ -77,63 +80,33 @@ export class MyComponent {
   //   }
   // }
 
-  copyToClipboard() {
-    const blob = new Blob(this.copiedData, { type: 'text/plain' });
-    const clipboardItem = new ClipboardItem({ 'text/plain': blob });
-    navigator.clipboard.write([clipboardItem]);
-    this.selectedColumnIndex = -1;
-    this.copiedData = [];
-  }
+  // copyToClipboard() {
+  //   const blob = new Blob(this.copiedData, { type: 'text/plain' });
+  //   const clipboardItem = new ClipboardItem({ 'text/plain': blob });
+  //   navigator.clipboard.write([clipboardItem]);
+  //   this.selectedColumnIndex = -1;
+  //   this.copiedData = [];
+  // }
 
-  getTableTd(td: any, th: string | number, index: number, indexSelection: number, rowOrColumn: string) {
-    if (this.selectedColumnIndex !== -1 && this.selectedColumnIndex === indexSelection) {
-      this.copiedData.push(`${td}'\n'`);
-    }
-    return (
-      <div
-        class={`table-data-td ${this.selectedColumnIndex === indexSelection ? 'selected-column' : ''}`}
-        contentEditable={this.editMode}
-        onClick={() => this.activateEditMode()}
-        onBlur={e => this.editTableData(e, td, th, index, rowOrColumn)}
-      >
-        {td}
-      </div>
-    );
-  }
+  // getTableTd(td: any, th: string | number, index: number, indexSelection: number, rowOrColumn: string) {
+  //   if (this.selectedColumnIndex !== -1 && this.selectedColumnIndex === indexSelection) {
+  //     this.copiedData.push(`${td}'\n'`);
+  //   }
+  //   return (
+  //     <div
+  //       class={`table-data-td ${this.selectedColumnIndex === indexSelection ? 'selected-column' : ''}`}
+  //       contentEditable={this.editMode}
+  //       onClick={() => this.activateEditMode()}
+  //       onBlur={e => this.editTableData(e, td, th, index, rowOrColumn)}
+  //     >
+  //       {td}
+  //     </div>
+  //   );
+  // }
 
   setColumnIndex(index: number) {
     this.selectedColumnIndex = index;
   }
-
-  handleScroll(e: any) {
-    const scrollTop = e.target.scrollTop;
-    if (scrollTop > this.lastScrollTop) {
-      this.endIndex = this.endIndex + 5;
-    } else {
-      if (this.endIndex >= 30) {
-        this.endIndex = this.endIndex - 5;
-      }
-    }
-    this.lastScrollTop = scrollTop === 0 ? 0 : scrollTop;
-  }
-
-  // componentDidRender() {
-  //   const observer = new IntersectionObserver(
-  //     entries => {
-  //       entries.forEach(entry => {
-  //         entry.target.classList.toggle('hide', entry.isIntersecting);
-  //         if (entry.isIntersecting) observer.unobserve(entry.target);
-  //       });
-  //     },
-  //     {
-  //       threshold: 1,
-  //     },
-  //   );
-  //   const rows = this.el.shadowRoot.querySelectorAll('.hide');
-  //   rows.forEach(element => {
-  //     observer.observe(element);
-  //   });
-  // }
 
   render() {
     return (
@@ -147,26 +120,42 @@ export class MyComponent {
         </div>
         <br />
         {this.results.length ? (
-          <div class="table-container" ref={el => (this.tableDivRef = el)} onScroll={e => this.handleScroll(e)}>
-            <div class="table-default-header-container">
-              <div class="table-data-index">#</div>
-              {this.getHeader(this.results[this.activeIndex][0].length).map((header: string, index: number) => (
-                <div onClick={() => this.setColumnIndex(index)} class="table-default-header">
-                  {header}
-                </div>
-              ))}
-            </div>
-            <div class="table-row-container" ref={el => (this.rowContainerRef = el)}>
-              {this.results[this.activeIndex].slice(this.startIndex, this.endIndex).map((e: any, i: number) => (
-                <div class="">
-                  <div class="table-data-container">
-                    <div class="table-data-index">{i + 1}</div>
-                    {Object.entries(e).map(([keys, values], index: number) => this.getTableTd(values, keys, i, index, 'row'))}
-                  </div>
-                </div>
-              ))}
-            </div>
+          //   <div class="table-container" ref={el => (this.tableDivRef = el)} onScroll={e => this.handleScroll(e)}>
+          //   <div class="table-default-header-container">
+          //     <div class="table-data-index">#</div>
+          //     {this.getHeader(this.results[this.activeIndex][0].length).map((header: string, index: number) => (
+          //       <div onClick={() => this.setColumnIndex(index)} class="table-default-header">
+          //         {header}
+          //       </div>
+          //     ))}
+          //   </div>
+          //   <div class="table-row-container" ref={el => (this.rowContainerRef = el)}>
+          //     {this.results[this.activeIndex].slice(this.startIndex, this.endIndex).map((e: any, i: number) => (
+          //       <div class="">
+          //         <div class="table-data-container">
+          //           <div class="table-data-index">{i + 1}</div>
+          //           {Object.entries(e).map(([keys, values], index: number) => this.getTableTd(values, keys, i, index, 'row'))}
+          //         </div>
+          //       </div>
+          //     ))}
+          //   </div>
+          // </div>
+          <div class="table-container">
+            <revo-grid
+              style={{ minHeight: '300px' }}
+              columns={Object.keys(this.results[this.activeIndex][0]).map(key => {
+                return { prop: key, name: key, size: 200 };
+              })}
+              source={this.results[this.activeIndex]}
+              resize={true}
+              rowHeaders={true}
+              range={true}
+              // @ts-ignore
+              // columnTypes={columnTypes}
+            />
           </div>
+        ) : this.loader ? (
+          <div>Loading...</div>
         ) : (
           <div>No Data</div>
         )}
